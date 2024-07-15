6450bab6193e29c933a821b3507d904b9c033d22
lakvedant <lakshitvedant1@gmail.com>
2024-07-15 12:46:58 +0530

    forth commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs/server';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/profile', '/register', '/how-it-works','/about-us','/hubs','/contact-us',]
+  publicRoutes: ['/', '/profile', '/register', '/how-it-works','/about-us','/hubs','/contact-us','/api/webhooks/clerk']
 });
 
 export const config = {
